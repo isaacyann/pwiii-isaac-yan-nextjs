1ec0ccdb06247f5e24ecfe6894e6f852d793764a
Isaac <isaacyan21@gmail.com>
2025-04-14 22:10:21 -0300

    Revendo a documentação de NextJS

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -38,9 +38,9 @@ export default function NavLinks() {
               },
             )}
           >
-          <LinkIcon className="w-6" />
-          <p className="hidden md:block">{link.name}</p>
-        </Link>
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
         );
       })}
     </>
